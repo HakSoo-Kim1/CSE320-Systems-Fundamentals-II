5d8c0c946c6f21e60578b7b955858446217288da
Hak Soo Kim <haksokim@cs.stonybrook.edu>
2022-04-24 23:51:18 -0400

    Finish main.c and server.c

--- hw5/src/main.c
+++ hw5/src/main.c
@@ -4,8 +4,11 @@
 #include "pbx.h"
 #include "server.h"
 #include "debug.h"
+#include "csapp.h"
 
 static void terminate(int status);
+void SIGHUP_handler(int sig);
+volatile sig_atomic_t terminateFlag = 0;
 
 /*
  * "PBX" telephone exchange simulation.
@@ -16,9 +19,39 @@ int main(int argc, char* argv[]){
     // Option processing should be performed here.
     // Option '-p <port>' is required in order to specify the port number
     // on which the server should listen.
+    
+    char *port = NULL;    // store port num
+    int opt;
+    opterr = 0;         // prevent the error message from getopt 
 
     // Perform required initialization of the PBX module.
-    debug("Initializing PBX...");
+
+    while((opt = getopt(argc, argv, "p:")) != -1){
+    switch(opt){
+      case 'p':                       // port command is given
+        debug("p command is given");
+        port = optarg;                // store given port
+        break;
+      default:
+        fprintf(stderr, "Usage: pbx -p <port>\n");
+        exit(EXIT_FAILURE);
+    }
+  }
+
+    if (argc != 3 || port == NULL) {
+        fprintf(stderr, "Usage: pbx -p <port>\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // debug("%d",atoi(optarg));
+
+    if ((atoi(port)) < 1024) {
+        fprintf(stderr, "Wrong port format (should be greater than or equal to 1024)\n");
+        exit(EXIT_FAILURE);
+    }
+
+    debug("Initializing PBX... with port : %s", port);
+
     pbx = pbx_init();
 
     // TODO: Set up the server socket and enter a loop to accept connections
@@ -27,10 +60,24 @@ int main(int argc, char* argv[]){
     // a SIGHUP handler, so that receipt of SIGHUP will perform a clean
     // shutdown of the server.
 
-    fprintf(stderr, "You have to finish implementing main() "
-	    "before the PBX server will function.\n");
+    Signal(SIGHUP, SIGHUP_handler);
 
-    terminate(EXIT_FAILURE);
+    int listenfd, *connfdp;
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+    pthread_t tid;
+    listenfd = Open_listenfd(port);
+
+    while (terminateFlag == 0) {
+    clientlen=sizeof(struct sockaddr_storage);
+    connfdp = Malloc(sizeof(int));
+    *connfdp = Accept(listenfd, (SA *) &clientaddr, &clientlen);
+    Pthread_create(&tid, NULL, pbx_client_service, connfdp);
+    }
+
+    debug("\n\n main terminating ");
+
+    terminate(EXIT_SUCCESS);
 }
 
 /*
@@ -41,4 +88,9 @@ static void terminate(int status) {
     pbx_shutdown(pbx);
     debug("PBX server terminating");
     exit(status);
+}
+
+void SIGHUP_handler(int sig) {
+  terminateFlag = 1;
+  debug("Handler triggered");
 }
--- hw5/src/server.c
+++ hw5/src/server.c
@@ -3,19 +3,134 @@
  * Manages interaction with a client telephone unit (TU).
  */
 #include <stdlib.h>
+#include <string.h>
 
 #include "debug.h"
 #include "pbx.h"
 #include "server.h"
+#include "csapp.h"
+
+
+
+char *commandStrGenerator(char* command);
 
 /*
  * Thread function for the thread that handles interaction with a client TU.
  * This is called after a network connection has been made via the main server
  * thread and a new thread has been created to handle the connection.
  */
-#if 0
 void *pbx_client_service(void *arg) {
-    // TO BE IMPLEMENTED
-    abort();
+
+    char* pickupCommand = commandStrGenerator(tu_command_names[TU_PICKUP_CMD]); 
+    char* hangupCommand = commandStrGenerator(tu_command_names[TU_HANGUP_CMD]); 
+    char* dialCommand = commandStrGenerator(tu_command_names[TU_DIAL_CMD]); 
+    char* chatCommand = commandStrGenerator(tu_command_names[TU_CHAT_CMD]); 
+
+    int connfd = *((int *)arg);
+    Free(arg);
+    Pthread_detach(pthread_self());
+    TU * newTu = tu_init(connfd);
+    pbx_register(pbx, newTu, connfd);
+    FILE *fp = Fdopen(connfd, "r");
+    
+    while(1){       // service loop
+        int counter = 0;
+        char *buf =  (char *) Malloc(MAXBUF);
+        int bufSize = MAXBUF;
+        char ch;
+        while ((ch = fgetc(fp)) != EOF) {
+            if ((sizeof(char) * (counter + 1)) >= bufSize){ // more buffer required
+                bufSize *= 2;
+                buf = Realloc(buf, bufSize);
+            }
+            buf[counter] = ch;
+            if ((counter >= 1) && (ch == '\n') && (buf[counter - 1] == '\r')){   // command end
+                buf[counter + 1] = '\0';
+                break;
+            }
+            counter += 1;
+        }
+        if (ch == EOF){
+            Free(buf);
+            break;
+        }
+        
+        debug("\tgiven string is : %s",buf);
+
+        if (strcmp(buf, pickupCommand) == 0){
+            tu_pickup(newTu);
+        }   
+        else if (strcmp(buf, hangupCommand) == 0){
+            tu_hangup(newTu);
+        }
+        else{
+            buf[counter - 1] = '\0';
+            char *ptr;
+            if ((ptr = strstr(buf, dialCommand))!= NULL){
+                if (ptr == buf){
+                    int dialStart = strlen(dialCommand);
+                    if (buf[dialStart] == '\0'){
+                        debug("no dial given");         /// maybe not necessary
+                        pbx_dial(pbx, newTu, -1);       /// maybe not necessary
+                    }
+                    else{
+                        debug("%s",&buf[dialStart]);
+                        int dialNum = atoi(&buf[dialStart]);
+                        debug("dialNum is %d",dialNum); 
+                        pbx_dial(pbx, newTu, dialNum);
+                    }
+                }
+            }
+            else if ((ptr = strstr(buf, chatCommand)) != NULL){
+                if (ptr == buf){
+                    int chatStart = strlen(chatCommand);
+                    if (buf[chatStart] == '\0'){
+                        tu_chat(newTu, " ");
+                    }
+                    else{
+                        tu_chat(newTu, &buf[chatStart]);
+                    }
+                    
+                }
+            }
+            else{
+                debug("WRONG COMMAND : %s",buf);
+            }
+        }
+        Free(buf);
+    }
+    
+
+    Free(pickupCommand);
+    Free(hangupCommand);
+    Free(dialCommand);
+    Free(chatCommand);
+    pbx_unregister(pbx, newTu);
+    Close(connfd);
+
+    return NULL;
 }
-#endif
+
+char *commandStrGenerator(char* command){
+    int commandLen = strlen(command);
+    char* commandStr;
+    if ( strcmp(command, tu_command_names[TU_PICKUP_CMD]) == 0 || strcmp(command, tu_command_names[TU_HANGUP_CMD]) == 0){
+        commandStr = Malloc(sizeof(char) * (commandLen + 3));
+        strcpy(commandStr, command);
+        commandStr[commandLen] = '\r';
+        commandStr[commandLen + 1] = '\n';
+        commandStr[commandLen + 2] = '\0';
+        return commandStr;
+    }
+    else if(strcmp(command, tu_command_names[TU_DIAL_CMD]) == 0 || strcmp(command, tu_command_names[TU_CHAT_CMD]) == 0){
+        commandStr = Malloc(sizeof(char) * (commandLen + 2));
+        strcpy(commandStr, command);
+        commandStr[commandLen] = ' ';
+        commandStr[commandLen + 1] = '\0';
+        return commandStr;
+    }
+    return NULL;
+}
+
+// gdb --args path/to/exe arg1 arg2 arg3
+// 
